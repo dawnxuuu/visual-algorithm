9656eebf3d7bb67f3a35093ba09e068b458c5d07
徐晓 <xu.xiao@zhaopin.com.cn>
2021-03-02 14:19:50 +0800

    add canvas demo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <wrapper></wrapper>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Wrapper from './components/Wrapper.vue'
+
+export default {
+  components: {Wrapper}
+}
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
