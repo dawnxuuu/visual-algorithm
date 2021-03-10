0ab0fbf86b0293864bd133c7d6ba15f9c6951b40
徐晓 <xu.xiao@zhaopin.com.cn>
2021-03-10 12:53:43 +0800

    add class draw

--- src/components/wrapper.vue
+++ src/components/wrapper.vue
@@ -5,8 +5,6 @@
 </template>
 
 <script>
-import CanvasDraw from './CanvasDraw'
-
 export default {
   name: 'Wrapper',
   data () {
@@ -15,7 +13,16 @@ export default {
     }
   },
   mounted () {
-    new CanvasDraw('canvasContainer')
+    const canvasContainer = document.getElementById('canvasContainer')
+    const ctx = canvasContainer.getContext("2d")
+
+    ctx.fillStyle = "#1a0701";
+    ctx.fillRect (10, 10, 10, 200);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+
+    console.log('=xu=', ctx)
   },
 }
 </script>
